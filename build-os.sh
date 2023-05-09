7ba481cc0acdb92600c5cb1258f28cd1196aa8a6
SaadZellag <saad.zellag@gmail.com>
2023-05-09 17:30:54 -0400

    cleanup

--- build-os.sh
+++ build-os.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/sh
 set -e
-cd os
-cargo rustc --target-dir ../target --release -- --emit=obj
-cd ..
-
-# nasm bootloader.asm -f bin -o ../../bmc-os.img
-
-# nasm mbr.asm -f bin -o ../../bmc-os.img
-
-# ld -m elf_i386 -o /tmp/kernel.bin -Ttext 0x1000 target/x86/debug/os --oformat binary
+cargo rustc --bin main --release -- --emit=obj
 
 cd src/asm
 nasm -f elf32 bootloader.asm -f bin -o /tmp/boot_sect.bin
@@ -17,13 +9,4 @@ cd ../..
 ld -m elf_i386 -o /tmp/kernel.bin -Ttext 0x1000 /tmp/kernel_entry.bin target/x86/release/deps/*.o --ignore-unresolved-symbol _GLOBAL_OFFSET_TABLE_ --oformat binary
 cat /tmp/boot_sect.bin /tmp/kernel.bin > bmc-os.img
 
-
-# nasm mbr.asm -f bin -o /tmp/mbr.bin
-# nasm bootloader.asm -f elf -o /tmp/bootloader.bin
-# cd ../..
-# ld -m elf_i386 -o /tmp/kernel.bin -Ttext 0x1000 /tmp/bootloader.bin target/x86/debug/os --oformat binary
-# cat /tmp/mbr.bin /tmp/kernel.bin > bmc-os.img
-
 dd if=/dev/zero of=bmc-os.img bs=1 count=1 seek=524287
-
-# cargo run --release --bin assembleos target/x86_64/debug/os
